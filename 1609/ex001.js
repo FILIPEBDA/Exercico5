b19acf4cf25b1c50c570a11d1197fadb09e790f8
FILIPEBDA <143577668+FILIPEBDA@users.noreply.github.com>
2023-09-16 12:35:21 -0300

    Add files via upload

---
+++ 1609/ex001.js
@@ -0,0 +1,13 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync()
+
+function desconto (Valor_Bruto){
+
+    let ValorAjustado
+
+    if(Valor_Bruto < 1000){
+       ValorAjustado = Valor_Bruto
+    }
+
+    else if 
+}
